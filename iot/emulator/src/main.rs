b5cc70c38ea94ce9236fec2273c9646ae763ef71
vinogradnick <vinogradovnick32@gmail.com>
2023-10-18 18:07:24 +0300

    rewrite ssl

--- iot/emulator/src/main.rs
+++ iot/emulator/src/main.rs
@@ -1,27 +1,56 @@
-mod devices;
-mod utils;
+// Импорт необходимых пакетов
+use heapless::Vec;
+use serde::{Deserialize, Serialize};
+use serde_json_core::{de, ser};
 
-use std::time::Duration;
+extern crate mio_httpc;
+use mio_httpc::CallBuilder;
 
-use crate::devices::device::Device;
-use crate::devices::traffic::data::TrafficLightData;
+// Определение структуры данных
+#[derive(Debug, Serialize, Deserialize)]
+struct SensorData {
+    temperature: f32,
+    humidity: f32,
+}
 
-use tokio::{task, time};
+//https://lib.rs/crates/mio_httpc
 
-#[tokio::main]
-async fn main() {
-    let device = Device::create_fake(devices::device::DeviceType::TrafficLight);
-    device.save_config().await;
-    let data = TrafficLightData::create_fake(&device);
-    let output_format = serde_json::to_string_pretty(&data).unwrap();
+fn main() {
+    // Создание экземпляра структуры SensorData
+    let sensor_data = SensorData {
+        temperature: 25.5,
+        humidity: 60.2,
+    };
 
-    let forever = task::spawn(async {
-        let mut interval = time::interval(Duration::from_millis(10));
+    // Сериализация структуры в байтовый массив
+    let serializer: heapless::String<64> = ser::to_string(&sensor_data).unwrap();
 
-        loop {
-            interval.tick().await;
-        }
-    });
+    // Вывод сериализованных данных
+    println!("Serialized: {:?}", serializer);
 
-    forever.await;
+    // Десериализация байтового массива в структуру
+    let deserializer = de::from_str::<SensorData>(&serializer).unwrap();
+
+    println!("Deserialized: {:?}", deserializer);
+    // Создание экземпляра Poll для мониторинга событий ввода/вывода
+
+    let (response_meta, body) = CallBuilder::get()
+        .timeout_ms(5000)
+        .url("http://www.example.com")
+        .unwrap()
+        .exec()
+        .unwrap();
+
+    // With URL construction.
+    // This way of building the URL is highly recommended as it will always result in correct
+    // values by percent encoding any URL unsafe characters.
+    // This calls: https://www.example.com/a/b?key1=val1
+    let (response_meta, body) = CallBuilder::get()
+        .timeout_ms(5000)
+        .host("www.example.com")
+        .path_segm("a")
+        .path_segm("b")
+        .query("key1", "val1")
+        .exec()
+        .unwrap();
 }
